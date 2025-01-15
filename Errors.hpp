f1e9f9af9ba617df45f610ca521ff207387ffd3e
Gabarnou <146858389+Hewya@users.noreply.github.com>
2025-01-15 16:22:33 +0100

    Changing parameters types

--- Errors.hpp
+++ Errors.hpp
@@ -1,9 +1,6 @@
 #ifndef ERRORS_HPP
 #define ERRORS_HPP
 
-#include "Client.hpp"
-#include "Channel.hpp"
-
 #include <string>
 #include <iostream>
 #include <sys/socket.h>
@@ -11,39 +8,39 @@
 
 #define SERVER_NAME "IRCserv"
 
-void	error_443(Client* client, Channel* channel);
-void	error_441(Client* client, Channel* channel);
-void	error_476(Client* client, Channel* channel);
-void	error_403(Client* client, Channel* channel);
-void	error_442(Client* client, Channel* channel);
-void	error_461(Client* client, std::string command);
-void	error_482(Client* client, Channel* channel);
-void	error_501(Client* client);
-void	error_431(Client* client);
-void	error_432(Client* client, std::string target_nick);
-void	error_433(Client* client, std::string target_nick);
-void	error_436(Client* client, std::string target_nick);
-void	error_401(Client* client, std::string target_nick);
-void	error_402(Client* client, std::string target_server);
-void	error_404(Client* client, Channel* channel);
-void	error_411(Client* client);
-void	error_412(Client* client);
-void	error_462(Client* client);
-void	error_405(Client* client, Channel* channel);
-void	error_406(Client* client, std::string target_nick);
-void	error_409(Client* client);
-void	error_417(Client* client);
-void	error_421(Client* client);
-void	error_451(Client* client);
-void	error_464(Client* client);
-void	error_465(Client* client);
-void	error_471(Client* client, Channel* channel);
-void	error_472(Client* client, std::string mode_char);
-void	error_473(Client* client, Channel* channel);
-void	error_474(Client* client, Channel* channel);
-void	error_475(Client* client, Channel* channel);
-void	error_483(Client* client);
-void	error_491(Client* client);
+void	error_443(int fd, std::string client, std::string channel);
+void	error_441(int fd, std::string client, std::string channel);
+void	error_476(int fd, std::string client, std::string channel);
+void	error_403(int fd, std::string client, std::string channel);
+void	error_442(int fd, std::string client, std::string channel);
+void	error_461(int fd, std::string client, std::string command);
+void	error_482(int fd, std::string client, std::string channel);
+void	error_501(int fd, std::string client);
+void	error_431(int fd, std::string client);
+void	error_432(int fd, std::string client, std::string target_nick);
+void	error_433(int fd, std::string client, std::string target_nick);
+void	error_436(int fd, std::string client, std::string target_nick);
+void	error_401(int fd, std::string client, std::string target_nick);
+void	error_402(int fd, std::string client, std::string target_server);
+void	error_404(int fd, std::string client, std::string channel);
+void	error_411(int fd, std::string client);
+void	error_412(int fd, std::string client);
+void	error_462(int fd, std::string client);
+void	error_405(int fd, std::string client, std::string channel);
+void	error_406(int fd, std::string client, std::string target_nick);
+void	error_409(int fd, std::string client);
+void	error_417(int fd, std::string client);
+void	error_421(int fd, std::string client);
+void	error_451(int fd, std::string client);
+void	error_464(int fd, std::string client);
+void	error_465(int fd, std::string client);
+void	error_471(int fd, std::string client, std::string channel);
+void	error_472(int fd, std::string client, std::string mode_char);
+void	error_473(int fd, std::string client, std::string channel);
+void	error_474(int fd, std::string client, std::string channel);
+void	error_475(int fd, std::string client, std::string channel);
+void	error_483(int fd, std::string client);
+void	error_491(int fd, std::string client);
 
 
 #endif
